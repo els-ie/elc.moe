a9b2f5c834307ee3afbfe1f4c620b3d27ff0fb64
els-ie <elsie.heck@gmail.com>
2026-07-26 12:13:24 -0600

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
+
+  let { children } = $props();
+</script>
+
+<svelte:head
+  ><meta name="viewport" content="width=device-width, initial-scale=1.0" /><link
+    rel="icon"
+    href={favicon}
+  /></svelte:head
+>
+<title>Welcome to elc.moe!</title>
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+</script>
+
+<div class="bg-gradient w-screen h-screen overflow-hidden -z-10"></div>
+<body>
+  <div
+    class="relative w-full h-200 sm:pl-[13%] justify-center sm:justify-start flex items-center align-bottom z-10"
+  >
+    <div
+      class="absolute inline-block mix-blend-screen text-9xl font-mclaren tracking-[22px] sm:tracking-[69px] hollow-text select-none"
+      aria-hidden="true"
+    >
+      elsie
+    </div>
+
+    <div
+      class="absolute inline-block mix-blend-color-dodge text-9xl font-mclaren tracking-[22px] sm:tracking-[69px] [text-shadow:1px_2px_0px_oklch(0.8812_0.0732__59.26/80%)]"
+    >
+      elsie
+    </div>
+
+    <div
+      class="absolute inline-block text-transparent bg-clip-text bg-linear-to-b mix-blend-lighten text-center justify-start to-elsielightgreen
+        from-49% from-elsiedarkgreen hover:bg-linear-to-b hover:to-elsiehoverlightgreen hover:from-elsiehovergold
+        hover:from-5% hover:to-80% text-9xl font-mclaren tracking-[22px] sm:tracking-[69px] select-none"
+      aria-hidden="true"
+      style="filter:blur(6px)"
+    >
+      elsie
+    </div>
+  </div>
+</body>
+
+<style>
+  .bg-gradient {
+    position: absolute;
+    overflow: hidden;
+    /* Base gradient layout */
+    background: radial-gradient(
+      circle at 30% 30%,
+      rgb(17, 32, 34),
+      rgb(2, 19, 28) 77%
+    );
+  }
+</style>
