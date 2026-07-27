4a0b74277fbdf8488900aeb04543848fbfe4b246
els-ie <elsie.heck@gmail.com>
2026-07-27 00:23:24 -0600

    Made more responsive & added under construction notice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,56 @@
 <script lang="ts">
+  // import stars from "$lib/assets/stars.webm";
 </script>
 
-<div class="bg-gradient w-screen h-screen overflow-hidden -z-10"></div>
+<!-- <video -->
+<!--   src={stars} -->
+<!--   class="absolute w-screen mix-blend-screen -z-9" -->
+<!--   muted -->
+<!--   autoplay -->
+<!--   loop -->
+<!-- ></video> -->
+<!---->
+<div
+  class="absolute bg-custom-radial w-screen h-screen overflow-hidden -z-10"
+></div>
+
 <body>
-  <div
-    class="relative w-full h-200 sm:pl-[13%] justify-center sm:justify-start flex items-center align-bottom z-10"
-  >
+  <div class="container relative w-dvw h-dvh">
+    <!-- Elsie  -->
     <div
-      class="absolute inline-block mix-blend-screen text-9xl font-mclaren tracking-[22px] sm:tracking-[69px] hollow-text select-none"
-      aria-hidden="true"
+      class="relative w-full h-[15%] sm:h-[30%] justify-center sm:justify-start sm:max-lg:px-[12%] lg:px-40 flex items-end z-10"
     >
-      elsie
-    </div>
+      <div
+        class="elsie-text mix-blend-screen hollow-text select-none"
+        aria-hidden="true"
+      >
+        elsie
+      </div>
 
-    <div
-      class="absolute inline-block mix-blend-color-dodge text-9xl font-mclaren tracking-[22px] sm:tracking-[69px] [text-shadow:1px_2px_0px_oklch(0.8812_0.0732__59.26/80%)]"
-    >
-      elsie
-    </div>
+      <div
+        class="elsie-text mix-blend-color-dodge [text-shadow:1px_2px_0px_oklch(0.8812_0.0732__59.26/80%)] select-none"
+        aria-hidden="true"
+      >
+        elsie
+      </div>
 
+      <div
+        class="elsie-text text-transparent bg-clip-text bg-linear-to-b mix-blend-lighten to-elsielightgreen
+        from-49% from-elsiedarkgreen hover:from-elsiehovergold
+        hover:from-5% hover:to-80%"
+        style="filter:blur(6px)"
+      >
+        elsie
+      </div>
+    </div>
+    <div class="h-10"></div>
     <div
-      class="absolute inline-block text-transparent bg-clip-text bg-linear-to-b mix-blend-lighten text-center justify-start to-elsielightgreen
-        from-49% from-elsiedarkgreen hover:bg-linear-to-b hover:to-elsiehoverlightgreen hover:from-elsiehovergold
-        hover:from-5% hover:to-80% text-9xl font-mclaren tracking-[22px] sm:tracking-[69px] select-none"
-      aria-hidden="true"
-      style="filter:blur(6px)"
+      class="flex h-0 sm:pl-70 md:pl-100 lg:pl-115 sm:justify-start justify-center items-center text-orange-300 text-4xl text-nowrap [text-shadow:1px_2px_0px_rgb(200_100_171)]"
     >
-      elsie
+      ✦ Under construction ✦
     </div>
   </div>
 </body>
 
 <style>
-  .bg-gradient {
-    position: absolute;
-    overflow: hidden;
-    /* Base gradient layout */
-    background: radial-gradient(
-      circle at 30% 30%,
-      rgb(17, 32, 34),
-      rgb(2, 19, 28) 77%
-    );
-  }
 </style>
