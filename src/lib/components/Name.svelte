146fed7bd1cf81f68c19becc336e9906ba8aee61
els-ie <elsie.heck@gmail.com>
2026-08-05 11:03:50 -0600

    Changed glow direction

--- src/lib/components/Name.svelte
+++ src/lib/components/Name.svelte
@@ -7,74 +7,80 @@ function onpointermove(event: PointerEvent) {
   coordinates.x = (event.clientX - rect.left);
   coordinates.y = (event.clientY - rect.top);
 }
+
+function onpointerleave() {
+  coordinates.x = 0;
+  coordinates.y = 0;
+}
 </script>
 
-<div class="stack">
+<div>
   <span
-    class="elsie-top"
+    class="elsie-header"
     {onpointermove}
+    {onpointerleave}
+    style="--x: {coordinates.x}rem; --y: {coordinates.y}rem"
     aria-hidden="true"
     >
-    elsie
-  </span>
-  <span class="elsie-bottom">
-    elsie
-  </span>
-  <span class="elsie-glow"
-  aria-hidden="true">
-    elsie
   </span>
 </div>
 
 <style>
-
-.stack {
+.elsie-header {
+	--text: "elsie";
+	position: relative;
   display: grid;
-  grid-template-areas: "stack";
-}
-
-.elsie-top, .elsie-bottom, .elsie-glow {
-  grid-area: stack;
-  place-self: center;
+  place-items: center;
 	font-family: "McLaren", sans-serif;
 	font-size: 8em;
-	letter-spacing: clamp(0.1em, 0.34em, 0.51em);
+	line-height: 1;
+	letter-spacing: var(--tracking, 0.1em);
+	@media screen and (min-width: 768px) {
+		--tracking: 0.34em;
+	}
+  
+  @media (prefers-reduced-motion: no-preference) {
+    transition: letter-spacing 0.2s ease-in;
   }
 
-.elsie-top {
-  text-shadow: 0.01em 0.015em 0em oklch(93.4% 0.0431 74.2 / 85%);
-  -webkit-text-stroke: 0.015em oklch(0.541 0.1313 283.19 / 39%);
-  color: oklch(19% 0.022 182);
-  mix-blend-mode: difference;
-}
+	&::before {
+		position: absolute;
+		content: var(--text);
+		text-shadow: 0.01em 0.015em 0em oklch(93.4% 0.0431 74.2 / 85%);
+		-webkit-text-stroke: 0.015em oklch(0.541 0.1313 283.19 / 39%);
+		color: oklch(19% 0.022 182);
+		mix-blend-mode: difference;
+	}
 
-.elsie-bottom {
-  background-clip: text;
-  -webkit-background-clip: text;
-  background-image: linear-gradient(
-    to bottom,
-    var(--elsie-darkgreen) 49%,
-    var(--elsie-lightgreen)
-  );
-  color: transparent;
-  mix-blend-mode: lighten;
-  filter: blur(0.04em);
+	&::after {
+		position: absolute;
+		content: var(--text);
+		background-clip: text;
+    -webkit-background-clip: text;
+		background-image: linear-gradient(
+			to bottom,
+			var(--elsie-darkgreen) 49%,
+			var(--elsie-lightgreen)
+		);
+		color: transparent;
+		mix-blend-mode: lighten;
+		filter: blur(0.04em);
+	}
 
-  &:hover {
+	&:hover::before {
+		text-shadow:
+			0.01em 0.015em 0em oklch(93.4% 0.0431 74.2 / 85%),
+			calc(var(--x) * -0.0045) calc(var(--y) * -0.012) 0.15em oklch(0.8848 0.0755 221.96 / 20%);
+
+	}
+
+	&:hover::after {
 		background-image: linear-gradient(
 			to bottom,
 			var(--elsie-hovergold) 5%,
 			var(--elsie-lightgreen) 80%
 		);
 		filter: blur(0.045em);
-  }
+	}
 }
-
-.elsie-glow {
-  color: #000;
-  text-shadow:
-    0.01em 0.015em 0em oklch(93.4% 0.0431 74.2 / 85%),
-    calc(var(--x) * 0.004) calc(var(--y) * 0.008) 0.15em oklch(0.8848 0.0755 221.96 / 90%);
-}
-
 </style>
